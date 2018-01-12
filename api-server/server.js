1c3cda0ba11385baf45df210007295d3ffed9ec8
whitefusion <whitefusion@163.com>
2018-01-12 18:13:51 +0800

    update dependencies

--- api-server/server.js
+++ api-server/server.js
@@ -14,6 +14,8 @@ const router = new Router()
 
 const Post = require('./db').Post
 
+
+
 // downstream error handling
 app.use(async (ctx,next) => {
   try{
@@ -29,12 +31,13 @@ app.use(async (ctx,next) => {
 app.use(logger())
 app.use(bodyParser())
 app.use(cors())
+
 app
 .use(router.routes())
 .use(router.allowedMethods())
 
 router
-.get('/',ctx=>ctx.body="hello world")
+.get('/',ctx=>ctx.body=help)
 .get('/categories',catCtrl.getCategories)
 .get('/:category/posts',postCtrl.getByCategory)
 .get('/posts',postCtrl.getPosts)
@@ -55,3 +58,97 @@ router
 app.listen(config.port, ()=>{
   console.log('Server listening on port %s, Ctrl+C to stop', config.port)
 })
+
+const help = `
+<pre>
+  Welcome to the Udacity Readable API!
+
+  Use an Authorization header to work with your own data:
+
+  fetch(url, { headers: { 'Authorization': 'whatever-you-want' }})
+
+  The following endpoints are available:
+
+  GET /categories
+    USAGE:
+      Get all of the categories available for the app. List is found in categories.js.
+      Feel free to extend this list as you desire.
+
+  GET /:category/posts
+    USAGE:
+      Get all of the posts for a particular category
+
+  GET /posts
+    USAGE:
+      Get all of the posts. Useful for the main page when no category is selected.
+
+  POST /posts
+    USAGE:
+      Add a new post
+
+    PARAMS:
+      id - UUID should be fine, but any unique id will work
+      timestamp - timestamp in whatever format you like, you can use Date.now() if you like
+      title - String
+      body - String
+      author - String
+      category: Any of the categories listed in categories.js. Feel free to extend this list as you desire.
+
+  GET /posts/:id
+    USAGE:
+      Get the details of a single post
+
+  POST /posts/:id
+    USAGE:
+      Used for voting on a post
+    PARAMS:
+      option - String: Either "upVote" or "downVote"
+
+  PUT /posts/:id
+    USAGE:
+      Edit the details of an existing post
+    PARAMS:
+      title - String
+      body - String
+
+  DELETE /posts/:id
+    USAGE:
+      Sets the deleted flag for a post to 'true'.
+      Sets the parentDeleted flag for all child comments to 'true'.
+
+  GET /posts/:id/comments
+    USAGE:
+      Get all the comments for a single post
+
+  POST /comments
+    USAGE:
+      Add a comment to a post
+
+    PARAMS:
+      id: Any unique ID. As with posts, UUID is probably the best here.
+      timestamp: timestamp. Get this however you want.
+      body: String
+      author: String
+      parentId: Should match a post id in the database.
+
+  GET /comments/:id
+    USAGE:
+      Get the details for a single comment
+
+  POST /comments/:id
+    USAGE:
+      Used for voting on a comment.
+
+  PUT /comments/:id
+    USAGE:
+      Edit the details of an existing comment
+
+    PARAMS:
+      timestamp: timestamp. Get this however you want.
+      body: String
+
+  DELETE /comments/:id
+    USAGE:
+      Sets a comments deleted flag to 'true'
+</pre>
+`
